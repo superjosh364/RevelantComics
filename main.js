7bfb24f3959ba03212b5390849ddbc5bfa9560b0
Bryce <brycegritter@gmail.com>
2017-02-16 20:34:22 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function newImage (in) {
+function newImage(in) {
  num = in;
- document.getElementById('image').src ='/RevelantComics/Images/TestImage' + num +'.png'
+ document.getElementById('image').src ='/RevelantComics/Images/TestImage' + num +'.png';
 }
