e1ebebd13874df30476a682d43c5a195accf7798
Bryce <brycegritter@gmail.com>
2017-02-16 20:56:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 function getImage(numIn) {
  num = numIn;
  document.getElementById('image').src ='/RevelantComics/Images/TestImage' + num +'.png';
-}
+};
+function getRollOverText(numIn) {
+ document.getElementById('image').title = MouseText['comic' + num].rollOver;
+};
