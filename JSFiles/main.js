836563e54180070b79306b308769e416c400e896
Bryce <gritter.development@gmail.com>
2017-02-17 22:11:54 -0700

    Update main.js

--- JSFiles/main.js
+++ JSFiles/main.js
@@ -6,5 +6,5 @@ function getRollOverText(numIn) {
  document.getElementById('image').title = MouseText['comic' + num].rollOver;
 };
 function getTitle(numIn) {
- document.getElementById('comicTitle').innerHtml = titles['comic' + num].Title;
+ document.getElementById('aboveComic').innerHtml = titles['comic' + num].Title;
 }; 
