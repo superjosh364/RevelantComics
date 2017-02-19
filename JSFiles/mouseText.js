7bcf51d75109e7cd08500698162294ae84b4f222
Bryce <gritter.development@gmail.com>
2017-02-18 21:47:17 -0700

    Update mouseText.js

--- JSFiles/mouseText.js
+++ JSFiles/mouseText.js
@@ -1,6 +1,6 @@
 var MouseText = {
   "comic1": {
-    "rollOver": "Text1"
+    "rollOver": "I mean, raising people from the dead? There's no way an all-powerful God could do that!"
   },
   "comic2": {
     "rollOver": "Text2"
