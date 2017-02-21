ce9f004699d427e7b483f43fcee2bbdebe2a6469
Bryce <gritter.development@gmail.com>
2017-02-21 13:51:11 -0700

    Update notes.js

--- JSFiles/notes.js
+++ JSFiles/notes.js
@@ -1,9 +1,9 @@
 var titles = {
   "comic1": {
-    "Notes": "First post! LOL. Seriously though, this is my first comic on the site. I think it's ok for a first comic. This was done digitally, which I'm using as an excuse for the quality. I either need to do them physically (i.e. on paper) and scan them or get a better stylus. I'm leaning towards the former, but we'll see. Anyway, hope you enjoy! URL for embedding: https://sdgcode.github.io/RevelantComics/Images/1.PNG"
+    "notes": "First post! LOL. Seriously though, this is my first comic on the site. I think it's ok for a first comic. This was done digitally, which I'm using as an excuse for the quality. I either need to do them physically (i.e. on paper) and scan them or get a better stylus. I'm leaning towards the former, but we'll see. Anyway, hope you enjoy! URL for embedding: https://sdgcode.github.io/RevelantComics/Images/1.PNG"
   },
   "comic2": {
-    "Notes": "" 
+    "notes": "" 
   }
 };
 
